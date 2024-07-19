b715056a91aa48087a2dc068ec06ebb309134483
damdupre <dupeo04@gmail.com>
2024-07-19 15:25:39 +0200

    Update AddLocalUsers.ps1

--- AddLocalUsers.ps1
+++ AddLocalUsers.ps1
@@ -1,6 +1,23 @@
 Write-Host "--- Début du script ---"
+#ajout de la fonction log
+function Log
+{
+    param([string]$FilePath,[string]$Content)
 
+    # Vérifie si le fichier existe, sinon le crée
+    If (-not (Test-Path -Path $FilePath))
+    {
+        New-Item -ItemType File -Path $FilePath | Out-Null
+    }
 
+    # Construit la ligne de journal
+    $Date = Get-Date -Format "dd/MM/yyyy-HH:mm:ss"
+    $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $logLine = "$Date;$User;$Content"
+
+    # Ajoute la ligne de journal au fichier
+    Add-Content -Path $FilePath -Value $logLine
+}
 Function Random-Password ($length = 6)
 {
     $punc = 46..46
@@ -66,5 +83,5 @@ foreach ($User in $Users)
 
 pause
 Write-Host "--- Fin du script ---"
+read-host "Appuyer sur ENTRE pour continuer ......."
 
-Start-Sleep -Seconds 10
